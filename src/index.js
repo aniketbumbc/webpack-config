566d545c66a1db4fca1b8a7342ee7348f5faaf04
Aniket B <aniket.umbc@gmail.com>
2023-07-14 13:27:50 +0530

    added babel config

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ const elvenShieldRecipe = {
 }
 
 
+const elvenGauntletRecipe = {
+    ...elvenShieldRecipe,
+    leather:1,
+    refinedMoonstone:4
+}
+
+
 console.log(elvenShieldRecipe);
+console.log("test",elvenGauntletRecipe);
