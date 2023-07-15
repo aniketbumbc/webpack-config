98ef621c049a112b6a3707622cc00d20a239b48f
Aniket B <aniket.umbc@gmail.com>
2023-07-15 17:25:00 +0530

    scss and css configuration

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const MiniCssExtractPlugin =  require("mini-css-extract-plugin");
+
+let mode = "development";
+
+if(process.env.NODE_ENV === "production"){
+  mode = "production";
+}
+
+
+module.exports = {
+  mode: mode,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test:/\.s?css$/i,
+        use:[MiniCssExtractPlugin.loader, "css-loader", "postcss-loader","sass-loader"],
+      }
+    ],
+  },
+
+  plugins:[new MiniCssExtractPlugin()],
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+};
