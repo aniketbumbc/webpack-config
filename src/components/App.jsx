cfbbde919f22ab515b65726b9dc4c1a26ea53af7
aniket <aniket.umbc@gmail.com>
2023-07-17 08:27:18 +0530

    Change app title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
-import "../styles/index.scss";
-import Recipes from "./Recipes";
-import sword from '../images/swc-sword.png'
-import swordSvg from '../images/sword.svg'
+import '../styles/index.scss';
+import Recipes from './Recipes';
+import sword from '../images/swc-sword.png';
+import swordSvg from '../images/sword.svg';
 
 const App = () => {
   return (
     <>
-      <section className="hero"> </section>
+      <section className='hero'> </section>
       <main>
         <section>
-          <h1>Oh hai react</h1>
+          <h1>Oh!! React Webpack App</h1>
         </section>
-        <img src={sword} alt="sword"  width={250}/>
-        <img src={swordSvg} alt="sword"  width={250}/>
-     
-      <Recipes/>
+        <img src={sword} alt='sword' width={250} />
+        <img src={swordSvg} alt='sword' width={250} />
+
+        <Recipes />
       </main>
     </>
   );
